f3841512378a24bfca8141f14675d44f545781a9
Jared Perkins <jared.perkins@hotmail.com>
2025-12-14 14:36:19 -0500

    Initial commit. Created a title screen for the program.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Using System;
+
+namespace CypherSystemCharacterGenerator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.CLear();
+            Console.WriteLine("Cypher System");
+            Console.WriteLine("Character Generator");
+            Console.WriteLine("by Jared Perkins\n");
+
+            
+        }
+    }
+}
